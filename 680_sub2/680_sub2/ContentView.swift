1824edb380c9427cc0643fc7dd5159bddca04530
ethan-vocal <ethanthevocal@gmail.com>
2023-12-13 06:36:45 -0800

    Creating new Project

---
+++ 680_sub2/680_sub2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  680_sub2
+//
+//  Created by ethan vocal on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 680_sub2/680_sub2/_80_sub2App.swift
@@ -0,0 +1,17 @@
+//
+//  _80_sub2App.swift
+//  680_sub2
+//
+//  Created by ethan vocal on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct _80_sub2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
