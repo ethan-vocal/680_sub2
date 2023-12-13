3ace69f29de66a99cc8d25c9b2aa6376212b8999
ethan-vocal <ethanthevocal@gmail.com>
2023-12-13 10:12:10 -0800

    working on moves
    trying to figure out a way to implement the Move struct into the Pokemon enum and connect

--- 680_sub2/680_sub2/Moveset.swift
+++ 680_sub2/680_sub2/Moveset.swift
@@ -11,4 +11,16 @@ import Foundation
 struct Move{
     let name: String
     let damageRange: ClosedRange<Double>
+    
+}
+
+// according to: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/extensions/
+
+// we can use extensions to pre-Define instance methods for the moves
+
+extension Move {
+    static let ember = Move(name: "Ember", damageRange: 8...14)
+    static let metalClaw = Move(name: "Metal Claw", damageRange: 10...15)
+    static let flamethrower = Move(name: "Flamethrower", damageRange: 12...15)
+    static let dragonRage = Move(name: "Dragon Rage", damageRange: 10...12)
 }
--- 680_sub2/680_sub2/Pokemon.swift
+++ 680_sub2/680_sub2/Pokemon.swift
@@ -19,5 +19,30 @@ enum Pokemon: String, CaseIterable{
     case dratini
     case jigglypuff
     
+    // if theres a way to attach the "Image("CharmanderIcon") to "charmander" using this method
+    var imageName: String {
+        return self.rawValue
+    }
+    
+    var Moveset: [Move] {
+        switch self{
+        
+        // first error - Type Move has no member for all the names
+        
+        // FOR NOW ALL PKMN HAS SAME MOVES AS CHARMANDER
+        case .charmander:
+            return[.ember, .metalClaw, .flamethrower, .dragonRage]
+        case .bulbasaur:
+            return[.ember, .metalClaw, .flamethrower, .dragonRage]
+        case .squirtle:
+            return[.ember, .metalClaw, .flamethrower, .dragonRage]
+        case .pikachu:
+            return[.ember, .metalClaw, .flamethrower, .dragonRage]
+        case .dratini:
+            return[.ember, .metalClaw, .flamethrower, .dragonRage]
+        case .jigglypuff:
+            return[.ember, .metalClaw, .flamethrower, .dragonRage]
+        }
+    }
     
 }
